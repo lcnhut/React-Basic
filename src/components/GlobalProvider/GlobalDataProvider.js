4a0031cccd48342746d6c845b0faf56763e3c4f7
lcnhut <nhut.le@nfq.asia>
2022-06-03 12:56:58 +0700

    refactor: clean file

--- src/components/GlobalProvider/GlobalDataProvider.js
+++ src/components/GlobalProvider/GlobalDataProvider.js
@@ -1,20 +1,13 @@
 import React, { useContext, useState } from "react";
 import { userApi } from "../../apis";
-import checkIcon from "../../assets/check.svg";
-import errorIcon from "../../assets/error.svg";
-import infoIcon from "../../assets/info.svg";
-import warningIcon from "../../assets/warning.svg";
 
 const GlobalDataContext = React.createContext();
 
 function GlobalDataProvider(props) {
   const [isLoading, setIsLoading] = useState(false);
-  const [isNotify, setIsNotify] = useState(false);
-  const [toastProperties, setToastProperties] = useState({});
 
   const register = async () => {
     setIsLoading(true);
-    setIsNotify(false);
     const response = await userApi.register();
     if (response.status === 200) {
       setIsLoading(false);
@@ -41,60 +34,8 @@ function GlobalDataProvider(props) {
     }
   };
 
-  const showNoti = () => {
-    setIsNotify(true);
-  };
-
-  const hideNoti = () => {
-    setIsNotify(false);
-  };
-
-  const notification = {
-    success: (title, description) => {
-      setToastProperties({
-        id: Math.floor(Math.random() * 101 + 1),
-        title: title,
-        description: description,
-        backgroundColor: "#5cb85c",
-        icon: checkIcon,
-      });
-    },
-    error: (title, description) => {
-      setToastProperties({
-        id: Math.floor(Math.random() * 101 + 1),
-        title: title,
-        description: description,
-        backgroundColor: "#d9534f",
-        icon: errorIcon,
-      });
-    },
-    info: (title, description) => {
-      setToastProperties({
-        id: Math.floor(Math.random() * 101 + 1),
-        title: title,
-        description: description,
-        backgroundColor: "#5bc0de",
-        icon: infoIcon,
-      });
-    },
-    warn: (title, description) => {
-      setToastProperties({
-        id: Math.floor(Math.random() * 101 + 1),
-        title: title,
-        description: description,
-        backgroundColor: "#f0ad4e",
-        icon: warningIcon,
-      });
-    },
-  };
-
   const providerValues = {
     isLoading,
-    isNotify,
-    showNoti,
-    hideNoti,
-    toastProperties,
-    notification,
     register,
     login,
   };
