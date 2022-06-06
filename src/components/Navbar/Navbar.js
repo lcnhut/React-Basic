46cc8412c7ff046cb9b87799562fddb0163f6bcd
lcnhut <nhut.le@nfq.asia>
2022-06-06 16:15:27 +0700

    feat: keep active sidebar item after refreshing the page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,31 @@ import {
 } from "@ant-design/icons";
 import { Menu, Popconfirm } from "antd";
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const url = pathname.slice(1);
+
+  const handleActiveItem = (value) => {
+    switch (value) {
+      case "pet":
+        window.localStorage.setItem("activeSidebar", "2");
+        break;
+      case "setting":
+        window.localStorage.setItem("activeSidebar", "3");
+
+        break;
+      default:
+        window.localStorage.setItem("activeSidebar", "1");
+        break;
+    }
+  };
+  handleActiveItem(url);
+
+  const activeItem = window.localStorage.getItem("activeSidebar");
 
   const confirm = () => {
     navigate("/");
@@ -23,15 +43,19 @@ const Navbar = () => {
   const role = window.localStorage.getItem("userRole");
 
   return (
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={[activeItem.toString()]}
+    >
       <Menu.Item key={"1"} icon={<HomeOutlined />}>
         <NavLink to="dashboard">Dashboard</NavLink>
       </Menu.Item>
-      <Menu.Item icon={<TableOutlined />}>
+      <Menu.Item key={"2"} icon={<TableOutlined />}>
         <NavLink to="pet">Pet</NavLink>
       </Menu.Item>
       {role === "admin" && (
-        <Menu.Item icon={<SettingOutlined />}>
+        <Menu.Item key={"3"} icon={<SettingOutlined />}>
           <NavLink to="setting">Setting</NavLink>
         </Menu.Item>
       )}
